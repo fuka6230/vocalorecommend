b6428a13420f11d53f0ababde0472202362161ab
fuka <hakutomato17@gmail.com>
2022-01-04 17:18:13 +0900

    Add pg gem for

--- vocalo/urls.py
+++ vocalo/urls.py
@@ -5,9 +5,9 @@ from .import views
 app_name = 'vocalo'
 urlpatterns = [
     path('result/', views.result, name='result'),
-    path('home/', views.home, name='home'),
     path('tracks/', views.tracks, name='tracks'), 
     path('artists/', views.artists, name='artists'),
     path('form', views.form_name_view, name='form_name_view'),
-    path('form2', views.form_artist_view, name='form_artist_view')
+    path('form2', views.form_artist_view, name='form_artist_view'),
+    path('', views.home, name='home'),
 ]
